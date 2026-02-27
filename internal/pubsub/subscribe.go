db30e994f9be3e031a2d14f371f0312004689c1c
atulantonyz <40585728+atulantonyz@users.noreply.github.com>
2026-02-26 23:23:04 -0500

    set prefetch_count to speed up multiserver script

--- internal/pubsub/subscribe.go
+++ internal/pubsub/subscribe.go
@@ -76,6 +76,10 @@ func subscribe[T any](
 	if err != nil {
 		return err
 	}
+	err = ch.Qos(10, 0, false)
+	if err != nil {
+		return err
+	}
 	msgs, err := ch.Consume(queue.Name, "", false, false, false, false, nil)
 
 	if err != nil {
